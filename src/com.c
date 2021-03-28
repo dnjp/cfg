cff2da3e729fc5437f46edf4f38d151ecaa0b4f3
Daniel <dnjp@posteo.org>
2021-03-28 13:44:53 -0400

    adding filename parsing

--- src/com.c
+++ src/com.c
@@ -13,36 +13,50 @@
 int main(int argc, char **argv)
 {
 	char in[MAXBUF];
-	ssize_t len;
+	ssize_t in_len;
 	Array arr;
 	int ft_len;
 	int ft_idx;
 	bool should_comment;
+	int opt;
+	char *filename;
 
+	filename = NULL;
+	should_comment = false;
+	ft_len = sizeof(FileTypes)/sizeof(FileType);
+	
+	while((opt = getopt(argc, argv, ":f:")) != -1) 
+	{ 
+		switch(opt) 
+		{ 
+			case 'f': 
+				filename = optarg; 
+				break;
+		} 
+	} 
+	if(filename == NULL) {
+		fprintf(stderr, "filename must be provided with -f flag");
+		exit(-1);
+	}
 	if(array_create(&arr) != 0) {
 		fprintf(stderr, "could not allocate array");
 		exit(-1);
 	}
-
-	while((len = read(0, in, sizeof(in))) > 0) {
-		in[len] = '\0';
-		len++;
+	if((ft_idx = ft_parse(filename, FileTypes, ft_len)) >= 0) {
+		should_comment = true;
+	} 
+	while((in_len = read(0, in, sizeof(in))) > 0) {
+		in[in_len] = '\0';
+		in_len++;
 		char *t = strtok(in, "\n");
 		while(t != NULL) {
-			if(array_push(&arr, t, len) != 0) {
+			if(array_push(&arr, t, in_len) != 0) {
 				fprintf(stderr, "could not add item to array");
 				exit(-1);
 			}
 			t = strtok(NULL, "\n");
 		}
 	}
-
-	should_comment = false;
-	ft_len = sizeof(FileTypes)/sizeof(FileType);
-	if((ft_idx = ft_parse("bla.c", FileTypes, len)) >= 0) {
-		should_comment = true;
-	} 
-
 	for(int i = 0; i < arr.index; i++) {
 		char *t = (char*)malloc(MAXBUF*sizeof(char));
 		if(array_at(&arr, t, i) != 0) {
