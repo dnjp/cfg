8dbebce6ae95fa04e3a4c5d265442a5c63cb4cce
Daniel <dnjp@posteo.org>
2021-03-27 22:56:05 -0400

    create build setup

---
+++ src/com.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "config.h"
+
+int
+main(int argc, char **argv)
+{
+	printf("C_COM: %s\n", C_COM);
+	return 0;
+}
--- src/flags/flags.go
+++
@@ -1,7 +0,0 @@
-package flags
-
-import "flag"
-
-var Filename = flag.String("f", "", "file name to operate on")
-var Word = flag.String("w", "", "word to complete")
-var Unindent = flag.Bool("u", false, "unindent the buffer")
